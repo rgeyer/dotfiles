929beb763cc7fbb596d779180cc0201acb1e4ee8
Ryan J. Geyer <me@ryangeyer.com>
2012-08-14 10:43:07 -0700

    Added omz custom capability and phpenv goodies

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,3 +22,17 @@ for i in `find . -name ".*" | grep -v "\.git\|\.$"`; do
 	fi
 	ln -sf ${PWD}/$DOTFILE $HOME/$DOTFILE
 done
+
+if [ -d $HOME/.omz ]; then
+  cd "$(dirname "$0")/omz_custom"
+  for i in `find . | grep -v "\.$"`; do
+    DOTCUSTOMFILE=`basename "$i"`
+    CUSTOMFILE=$HOME/.omz/custom/$DOTCUSTOMFILE
+    CUSTOMFILEBKUP=$HOME/.omz/custom/${DOTCUSTOMFILE}_bkup
+    if [[ -e $CUSTOMFILE && ! -e $CUSTOMFILEBKUP ]]; then
+      echo "${warn}$CUSTOMFILE already existed, backing it up to $CUSTOMFILEBKUP before clobbering it${reset}"
+      mv $CUSTOMFILE $CUSTOMFILEBKUP
+    fi
+    ln -sf ${PWD}/$DOTCUSTOMFILE $CUSTOMFILE
+  done
+fi
